6582b904176d87d98bcc41859edc11928d75bb77
RITIK KOHLI <ritikkohli@gmail.com>
2022-08-08 18:22:18 +0530

    assignment get api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,35 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
-});
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.get('/GET/movies', function(req,res){
+    let arrMovies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(arrMovies)
 })
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+router.get('/GET/movies/:indexNumber', function(req,res){
+    let arrMovies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let index=req.params.indexNumber
+    if(index>3){
+        res.send('Use a valid index')
+    }
+    res.send(arrMovies[index])
 })
-
+router.get('/GET/films', function(req,res){
+    let arrFilms=[ {
+        id: 1,
+        name: 'The Shining'
+       },
+       {
+        id: 2,
+        name: 'Incendies'
+       },
+       {
+        id: 3,
+        name: 'Rang de basanti'
+       },
+       {
+        id: 4,
+        name: 'Finding Nemo'
+       }]
+    res.send(arrFilms)
+})
 module.exports = router;
