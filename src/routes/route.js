b1e45a15efbd5696328006f6ef32a62b39e29c8b
RITIK KOHLI <ritikkohli@gmail.com>
2022-08-09 21:58:40 +0530

    solution of missing element assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,37 +2,31 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+app.get("/sol1", function (req, res) {
 
-    res.send('My second ever api!')
+    let arr= [1,2,3,5,6,7]
+    let missingNumber
+    let total=0
+    arr.forEach(x=>total+=x)
+    let lastdigit=arr.pop()
+    let sum=lastdigit*(lastdigit+1)/2
+    missingNumber=sum-total 
+    res.send(  { data: missingNumber  }  );
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+app.get("/sol2", function (req, res) {
     
-    res.send('Dummy response')
-})
+    let arr= [33, 34, 35, 37, 38]
+    let missingNumber
+    let total=0;
+    arr.forEach(x=>total+=x)
+    let lengthWithMissed=arr.length+1
+    let lastdigit=arr.pop()
+    let firstdigit=arr[0]
+    let sum=lengthWithMissed*(firstdigit+lastdigit)/2
+    missingNumber=sum-total 
+
+    res.send(  { data: missingNumber  }  );
+});
 
 module.exports = router;
