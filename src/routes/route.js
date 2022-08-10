99abb5fea910865f3850c52595d0b09daf95764a
RITIK KOHLI <ritikkohli@gmail.com>
2022-08-10 13:31:12 +0530

    assignment get API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+let players=[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": ["swimming"]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": ["soccer"]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": ["soccer"]
+    }
+]
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
-});
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+router.post('/player',function(req,res){
+    let newPlayer=req.body
+    for(let i=0;i<players.length;i++){
+        if(players[i].name===newPlayer.name){
+            res.send("Already exist.")
+         
+        }        
+    }
+    players.push(newPlayer)
+    res.send( { data: players , status: true } )
 })
 
 module.exports = router;
