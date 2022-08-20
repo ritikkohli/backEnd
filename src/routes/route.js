ecc11c480de9de2a29a16bb5b92324b8105fbc6e
RITIK KOHLI <ritikkohli@gmail.com>
2022-08-20 12:32:17 +0530

    assignment on populate reference

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,15 +5,11 @@ const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
 const publisherController= require("../controllers/publisherController")
 
-router.post("/createAuthor", authorController.createAuthor  )
-
+router.post("/createAuthor", authorController.createAuthor)
 router.post("/createPublisher", publisherController.createPublisher)
-
-router.post("/createBook", bookController.createBook  )
-
+router.post("/createBook", bookController.createBook)
 router.get("/getBooks", bookController.getBooks)
-
-router.put("/updateBooks", bookController.updateBooks)
-// router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+router.put("/updateBooks", bookController.updateHardcover)
+router.put("/updateBooks", bookController.updatePrice)
 
 module.exports = router;
