c2b6bdd51f34ec34d312f6b8f1ddd7b4364354a7
RITIK KOHLI <ritikkohli@gmail.com>
2022-08-29 21:42:37 +0530

    assignment on auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,20 @@
 const jwt = require('jsonwebtoken')
 
-const validator = function(req,res,next){    
+const validator = function(req,res,next){
+    // validate header    
     if(!req.headers['x-auth-token'])
     return res.send('the request is missing token header')
-    
-    jwt.verify(req.headers['x-auth-token'],"very-secret-key",function(err,decoded){
-        if(err){
-            res.send("invailid token")
-        }
-    })
-    
-    next()
+    // verify token
+    let decoded = jwt.verify(req.headers['x-auth-token'],"very-secret-key")
+    if(!decoded){
+        res.send("Invailid token")
+    }
+    // check user has thier own token 
+    if(decoded.userId!=req.params.userId){
+        res.send("You can not access the details of another user")   
+    }else{
+        next()
+    }
 }
 
 module.exports.validator = validator
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require('express')
 const userController = require('../controller/userController.js')
 const auth = require('../middleware/auth.js')
 const router = express.Router()
 
 router.post("/registerUser", userController.createUser)
 router.post("/logIn", userController.logIn)
-router.get("/user/:userId",auth.validator,userController.fetchUser)
-router.put("/user/:userId", auth.validator, userController.updateUser)
-router.delete("/user/:userId", auth.validator, userController.deleteUser)
+router.get("/user/:userId", auth.validator , userController.fetchUser)
+router.put("/user/:userId", auth.validator , userController.updateUser)
+router.delete("/user/:userId", auth.validator , userController.deleteUser)
 
 module.exports = router;
